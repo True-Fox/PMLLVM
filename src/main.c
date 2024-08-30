d5f77b330136e831d1c5e506f2648a9b7052a587
True-Fox <pes1202100682@pesu.pes.edu>
2024-08-30 17:09:54 +0530

    Added arithmetic and logical instructions

--- src/main.c
+++ src/main.c
@@ -15,20 +15,33 @@ int stack[256];
 bool running = true;
 
 typedef enum {
-    PSH,
-    ADD,
-    POP,
-    SET,
+    PSH, ADD,
+    SUB, MUL,
+    DIV, MOD,
+    POP, SET,
+    AND, OR,
+    NOT,
     HLT
 } INST_SET;
 
 const int program[] = {
-    PSH, 5,
-    PSH, 6,
+    PSH, 8,
+    PSH, 3,
     ADD,
+    PSH, 2,
+    MUL,
+    PSH, 4,
+    MOD,
+    PSH, 1,
+    AND,
+    NOT,
+    PSH, 3,
+    OR,
     POP,
-    HLT
+    HLT         
 };
+
+
 
 int fetch() {
     return program[ip];
@@ -36,10 +49,12 @@ int fetch() {
 
 void eval(int instr) {
     switch(instr) {
+        //Machine Control Instructions
         case HLT: {
             running = false;
             break;
         }
+        //Stack Instructions
         case PSH: {
             if (sp < 255) {
                 sp++;
@@ -60,6 +75,7 @@ void eval(int instr) {
             }
             break;
         }
+        //Arithmetic Instructions
         case ADD: {
             if (sp >= 1) {
                 int a = stack[sp--];
@@ -72,9 +88,102 @@ void eval(int instr) {
                 running = false;
             }
             break;
+        }// ADD
+        case SUB:{
+            if(sp>=1){
+                int a = stack[sp--];
+                int b = stack[sp--];
+                int result = b - a;
+                sp++;
+                stack[sp] = result;
+            } else {
+                printf("Not enough values on stack to perform SUB\n");
+                running = false;
+            }
+            break;
+        }// SUB
+        case MUL:{
+                if(sp>=1){
+                int a = stack[sp--];
+                int b = stack[sp--];
+                int result = b * a;
+                sp++;
+                stack[sp] = result;
+                } else {
+                printf("Not enough values on stack to perform MUL\n");
+                }
+                break;
+        }// MUL 
+        case DIV:{
+                if(sp>=1){
+                int a = stack[sp--];
+                int b = stack[sp--];
+                if(a==0){
+                    printf("Division by zero\n");
+                    running = false;
+                }else{  
+                    int result = b/a;
+                    sp++;
+                    stack[sp] = result;
+                }              
+                } else {
+                printf("Not enough values on stack to perform DIV\n");
+                }
+                break;
+        }//DIV
+        case MOD:{
+                if(sp>=1){
+                    int a = stack[sp--];
+                    int b = stack[sp--];
+                    int result = b%a;
+                    sp++;
+                    stack[sp] = result;
+                } else {
+                    printf("Not enough values on stack to perform MOD\n");
+                }
+                break;
+        }//MOD
+        //Logical Instructions
+        case AND:{
+            if(sp >= 1){
+                int a = stack[sp--];
+                int b = stack[sp--];
+                int result = a&b;
+                sp++;
+                stack[sp] = result;
+            }else{
+                printf("Not enough values to perform AND\n");
+                running = false;
+            }
+            break;
+        }//AND
+        case OR:{
+            if(sp >= 1){
+                int a = stack[sp--];
+                int b = stack[sp--];
+                int result = a|b;
+                sp++;
+                stack[sp] = result;
+            }else{
+                printf("Not enough values on stack to perform OR\n");
+                running = false;
+            }
+            break;
+        }//OR
+        case NOT:{
+            if(sp>=1){
+                int a = stack[sp--];
+                int result = !a;
+                sp++;
+                stack[sp] = result;
+            }else{
+                printf("Not enough values on stack to perform NOT");
+                running = false;
+            }
+            break;
         }
-    }
-}
+    } // SWITCH
+} //EVAL
 
 int main() {
     while (running) {
